54bf96cc155a89494f9591789d08e88d1b81ecd5
Pierrot LC <pierrotlc@proton.me>
2024-06-18 11:48:38 +0200

    requirements

--- closeness.py
+++ closeness.py
@@ -1,18 +1,20 @@
- 
-import numpy as np
-import pickle
-import networkx as nx
-import torch
-from utils import *
-import random
-import torch.nn as nn
-from model_close import GNN_Close
-torch.manual_seed(20)
 import argparse
+import pickle
+import random
 
-#Loading graph data
+import networkx as nx
+import numpy as np
+import torch
+import torch.nn as nn
+
+from model_close import GNN_Close
+from utils import *
+
+torch.manual_seed(20)
+
+# Loading graph data
 parser = argparse.ArgumentParser()
-parser.add_argument("--g",default="SF")
+parser.add_argument("--g", default="SF")
 args = parser.parse_args()
 gtype = args.g
 print(gtype)
@@ -28,26 +30,32 @@ elif gtype == "GRP":
     print("Gaussian Random Partition graphs selected.")
 
 
-
-#Load training data
+# Load training data
 print(f"Loading data...")
-with open(data_path+"training.pickle","rb") as fopen:
-    list_graph_train,list_n_seq_train,list_num_node_train,cc_mat_train = pickle.load(fopen)
+with open(data_path + "training.pickle", "rb") as fopen:
+    list_graph_train, list_n_seq_train, list_num_node_train, cc_mat_train = pickle.load(
+        fopen
+    )
 
 
-with open(data_path+"test.pickle","rb") as fopen:
-    list_graph_test,list_n_seq_test,list_num_node_test,cc_mat_test = pickle.load(fopen)
+with open(data_path + "test.pickle", "rb") as fopen:
+    list_graph_test, list_n_seq_test, list_num_node_test, cc_mat_test = pickle.load(
+        fopen
+    )
 
 model_size = 10000
-#Get adjacency matrices from graphs
+# Get adjacency matrices from graphs
 print(f"Graphs to adjacency conversion.")
 
-list_adj_train,list_adj_mod_train = graph_to_adj_close(list_graph_train,list_n_seq_train,list_num_node_train,model_size)
-list_adj_test,list_adj_mod_test = graph_to_adj_close(list_graph_test,list_n_seq_test,list_num_node_test,model_size)
+list_adj_train, list_adj_mod_train = graph_to_adj_close(
+    list_graph_train, list_n_seq_train, list_num_node_train, model_size
+)
+list_adj_test, list_adj_mod_test = graph_to_adj_close(
+    list_graph_test, list_n_seq_test, list_num_node_test, model_size
+)
 
 
-
-def train(list_adj_train,list_adj_mod_train,list_num_node_train,cc_mat_train):
+def train(list_adj_train, list_adj_mod_train, list_num_node_train, cc_mat_train):
     model.train()
     total_count_train = list()
     loss_train = 0
@@ -60,17 +68,18 @@ def train(list_adj_train,list_adj_mod_train,list_num_node_train,cc_mat_train):
         adj_mod = adj_mod.to(device)
 
         optimizer.zero_grad()
-            
-        y_out = model(adj,adj_mod)
-        true_arr = torch.from_numpy(cc_mat_train[:,i]).float()
+
+        y_out = model(adj, adj_mod)
+        true_arr = torch.from_numpy(cc_mat_train[:, i]).float()
         true_val = true_arr.to(device)
-        
-        loss_rank = loss_cal(y_out,true_val,num_nodes,device,model_size)
+
+        loss_rank = loss_cal(y_out, true_val, num_nodes, device, model_size)
         loss_train = loss_train + float(loss_rank)
         loss_rank.backward()
         optimizer.step()
 
-def test(list_adj_test,list_adj_mod_test,list_num_node_test,bc_mat_test):
+
+def test(list_adj_test, list_adj_mod_test, list_num_node_test, bc_mat_test):
     model.eval()
     loss_val = 0
     list_kt = list()
@@ -78,46 +87,43 @@ def test(list_adj_test,list_adj_mod_test,list_num_node_test,bc_mat_test):
     for j in range(num_samples_test):
         adj = list_adj_test[j]
         adj_mod = list_adj_mod_test[j]
-        adj=adj.to(device)
+        adj = adj.to(device)
         adj_mod = adj_mod.to(device)
         num_nodes = list_num_node_test[j]
-        
-        y_out = model(adj,adj_mod)
-    
-        
-        true_arr = torch.from_numpy(cc_mat_test[:,j]).float()
+
+        y_out = model(adj, adj_mod)
+
+        true_arr = torch.from_numpy(cc_mat_test[:, j]).float()
         true_val = true_arr.to(device)
-    
-        kt = ranking_correlation(y_out,true_val,num_nodes,model_size)
+
+        kt = ranking_correlation(y_out, true_val, num_nodes, model_size)
         list_kt.append(kt)
 
+    print(
+        f"    Average KT score on test graphs is: {np.mean(np.array(list_kt))} and std: {np.std(np.array(list_kt))}"
+    )
 
-    print(f"    Average KT score on test graphs is: {np.mean(np.array(list_kt))} and std: {np.std(np.array(list_kt))}")
 
-
-
-#Model parameters
+# Model parameters
 hidden = 20
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = GNN_Close(ninput=model_size,nhid=hidden,dropout=0.6)
+model = GNN_Close(ninput=model_size, nhid=hidden, dropout=0.6)
 model.to(device)
 
-optimizer = torch.optim.Adam(model.parameters(),lr=0.0005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 num_epoch = 15
 
 print("Training")
 print(f"Number of epoches: {num_epoch}")
 for e in range(num_epoch):
     print(f"Epoch number: {e+1}/{num_epoch}")
-    train(list_adj_train,list_adj_mod_train,list_num_node_train,cc_mat_train)
+    train(list_adj_train, list_adj_mod_train, list_num_node_train, cc_mat_train)
 
-    #to check test loss while training
+    # to check test loss while training
     with torch.no_grad():
-        test(list_adj_test,list_adj_mod_test,list_num_node_test,cc_mat_test)
-#test on 10 test graphs and print average KT Score and its stanard deviation
-#with torch.no_grad():
+        test(list_adj_test, list_adj_mod_test, list_num_node_test, cc_mat_test)
+# test on 10 test graphs and print average KT Score and its stanard deviation
+# with torch.no_grad():
 #    test(list_adj_test,list_adj_mod_test,list_num_node_test,cc_mat_test)
 
-
-    
